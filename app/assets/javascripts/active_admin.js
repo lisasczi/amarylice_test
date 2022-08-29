74b8426fffe9085e9d5ef0f21d2c1d3ecfc40873
lisa scanzi <lisascanzi@gmail.com>
2022-08-29 17:05:33 +0200

    carrousel stimulus

--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -1,1 +1,9 @@
 //= require active_admin/base
+
+// app/javascript/controllers/index.js
+
+import { Application } from "stimulus"
+import Carousel from "stimulus-carousel"
+
+const application = Application.start()
+application.register("carousel", Carousel)
